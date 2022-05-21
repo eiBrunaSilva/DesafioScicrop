315b24fb60d440c6b090f778f341fff5b8b5c8a9
eiBrunaSilva <silvabruna.email@gmail.com>
2022-05-21 18:19:54 -0300

    map

--- listagem/listagem.js
+++ listagem/listagem.js
@@ -1,5 +1,5 @@
                                     //lati  //long  //zoom
-var map = new L.map('map').setView([51.505, -0.09], 2);
+var map = new L.map('map').setView([51.505, -0.09], 3);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
